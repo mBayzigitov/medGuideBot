32c5908b8c15803db69d14626b4f68213911d2e6
mBayzigitov <bayzigitov.murat@gmail.com>
2022-11-21 20:32:14 +0300

    project init

---
+++ src/main/java/pro/medguide/MedGuideTelegramBot/MedGuideTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package pro.medguide.MedGuideTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedGuideTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedGuideTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/medguide/MedGuideTelegramBot/MedGuideTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.medguide.MedGuideTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedGuideTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
